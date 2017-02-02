7cb10f5ee88b8fd77e76ae188827fd274ffdd946
Bruno Lemos <brunohplemos@gmail.com>
2017-02-02 18:32:23 -0200

    Showing static image on twitter

--- share/server.js
+++ share/server.js
@@ -29,6 +29,7 @@ var home = template({
 //
 app.express = express();
 app.express.use(compression());
+app.express.use(express.static('public'))
 
 // Set engine
 app.express.set('view engine', 'handlebars');
@@ -51,19 +52,19 @@ app.express.get('/:gif', apicache('10 minutes'), (req, res) => {
   // const uri = `http://share.isnowillegal.com/${gif}.gif`
   const uri = `https://storage.googleapis.com/is-now-illegal.appspot.com/gifs/${gif}.gif`;
 
-  res
-    .status(200)
-    .send(
-      template({
-        url: uri,
-        site: 'IsNowIllegal',
-        title: `${gif} Is Now Illegal!`,
-        gif_url: uri,
-        description: 'Declare things illegal and trump will sign it.',
-        content_type: 'video.other',
-        share_url: `http://isnowillegal.com/?${gif}`,
-      })
-    );
+  res.status(200).send(
+    template({
+      url: uri,
+      site: 'IsNowIllegal',
+      title: `${gif} Is Now Illegal!`,
+      gif_url: uri,
+      // TODO: this should be dynamic
+      gif_preview_url: 'http://share.isnowillegal.com/preview.png',
+      description: 'Declare things illegal and trump will sign it.',
+      content_type: 'video.other',
+      share_url: `http://isnowillegal.com/?${gif}`,
+    })
+  );
 });
 
 const PORT = process.env.PORT || 8080;
