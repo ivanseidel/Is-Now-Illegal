ef7cccf204c645d1cbb7c102c60fec9e47f634fc
Bruno Lemos <brunohplemos@gmail.com>
2017-02-03 02:57:19 -0200

    Fix footer padding (+added missing file)

---
+++ web/src/pages/MaintenancePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CenterBox from '../components/CenterBox';
+import H1 from '../components/H1';
+import Page from '../components/Page';
+
+export default () => (
+  <Page>
+    <CenterBox>
+      <H1>
+        We are having too much traffic. Please come back in a few minutes.
+      </H1>
+    </CenterBox>
+  </Page>
+);
