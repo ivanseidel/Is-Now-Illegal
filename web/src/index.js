145b4e1c548a985c1c6def70f93f49dde30cdfec
Bruno Lemos <brunohplemos@gmail.com>
2017-02-03 02:04:01 -0200

    Turn off maintenance mode

--- web/src/index.js
+++ web/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './reset.css';
 import './index.css';
 
-// import App from './App';
-import MaintenancePage from './pages/MaintenancePage';
+import App from './App';
+// import App from './pages/MaintenancePage';
 
-ReactDOM.render(<MaintenancePage />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
